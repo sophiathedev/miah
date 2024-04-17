5066112454b42a2a29c460444fccdccabc6be22e
iwannabethesophia <pydevmonkey4002@gmail.com>
2024-04-17 16:00:48 +0700

    repair: returned type spec for initialize mask king attack

--- lib/bitboard/king.ex
+++ lib/bitboard/king.ex
@@ -3,7 +3,7 @@ import Bitboard.Utils
 
 defmodule Bitboard.King do
   @doc "Initialize function for precalculate mask king attack"
-  @spec initialize_king_attack() :: list()
+  @spec initialize_king_attack() :: list(non_neg_integer())
   def initialize_king_attack do
     mask = 0..63 |> Enum.map(fn(pos) -> all_shift(bit_square(pos)) end) |> Enum.to_list
     mask
