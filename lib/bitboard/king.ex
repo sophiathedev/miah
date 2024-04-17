e4bb73afc1c97ebcee9033cb372de834c7020ae3
iwannabethesophia <pydevmonkey4002@gmail.com>
2024-04-17 15:42:39 +0700

    add: document for initialize king mask

--- lib/bitboard/king.ex
+++ lib/bitboard/king.ex
@@ -2,6 +2,7 @@
 import Bitboard.Utils
 
 defmodule Bitboard.King do
+  @doc "Initialize function for precalculate mask king attack"
   @spec initialize_king_attack() :: list()
   def initialize_king_attack do
     mask = 0..63 |> Enum.map(fn(pos) -> all_shift(bit_square(pos)) end) |> Enum.to_list
