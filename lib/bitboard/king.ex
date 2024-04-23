6fa5b8bb21206e27666df9f4aea70b10f95705e1
iwannabethesophia <pydevmonkey4002@gmail.com>
2024-04-23 23:39:21 +0700

    edit: using Stream instead Enum pipe for better performance at initialize_king_attacks()

--- lib/bitboard/king.ex
+++ lib/bitboard/king.ex
@@ -1,15 +1,13 @@
-#import Bitwise
-import Bitboard.Utils
-
 defmodule Bitboard.King do
   @moduledoc """
   This module is general set-wise for king, attacks, move and evaluation
   """
 
+  alias Bitboard.Utils
+
   @doc "Initialize function for precalculate mask king attack"
   @spec initialize_king_attack() :: list(non_neg_integer())
   def initialize_king_attack do
-    mask = 0..63 |> Enum.map(fn(pos) -> all_shift(bit_square(pos)) end) |> Enum.to_list
-    mask
+    0..63 |> Stream.map(&Utils.bit_square/1) |> Stream.map(&Utils.all_shift/1) |> Enum.to_list
   end
 end
