955b512597a3b1245bfded2872d4876a0901c34f
iwannabethesophia <pydevmonkey4002@gmail.com>
2024-04-23 23:15:29 +0700

    add: attacks mask for pawn format: inline function and reorder parameter for use pipe operator efficently

--- lib/bitboard/pawn.ex
+++ lib/bitboard/pawn.ex
@@ -7,30 +7,48 @@ defmodule Bitboard.Pawn do
 
   alias Bitboard.Utils
 
-  @compile {:inline, single_push_target: 3, double_push_target: 3}
+  @compile {:inline, single_push_target: 3, double_push_target: 3, east_attacks: 2, west_attacks: 2}
 
   @rank4 Utils.bit_rank(4)
   @rank5 Utils.bit_rank(5)
 
-  @doc "Mask for single pawn push"
-  @spec single_push_target(:black | :white, non_neg_integer(), non_neg_integer()) :: non_neg_integer()
-  def single_push_target(:white, pawns, empty) do
-    band(Utils.shift(pawns, :north), empty)
-  end
+  # General set-wise pawn operation
 
-  def single_push_target(:black, pawns, empty) do
-    band(Utils.shift(pawns, :south), empty)
-  end
+  @doc "Mask for single pawn push"
+  @spec single_push_target(non_neg_integer(), non_neg_integer(), :black | :white) :: non_neg_integer()
+  def single_push_target(pawns, empty, :white), do: band(Utils.shift(pawns, :north), empty)
+  def single_push_target(pawns, empty, :black), do: band(Utils.shift(pawns, :south), empty)
 
   @doc "Mask for double pawn push"
-  @spec double_push_target(:black | :white, non_neg_integer(), non_neg_integer()) :: non_neg_integer()
-  def double_push_target(:white, pawns, empty) do
-    single_push = single_push_target(:white, pawns, empty)
+  @spec double_push_target(non_neg_integer(), non_neg_integer(), :black | :white) :: non_neg_integer()
+  def double_push_target(pawns, empty, :white) do
+    single_push = single_push_target(pawns, empty, :white)
     Utils.shift(single_push, :north) &&& empty &&& @rank4
   end
 
-  def double_push_target(:black, pawns, empty) do
-    single_push = single_push_target(:black, pawns, empty)
+  def double_push_target(pawns, empty, :black) do
+    single_push = single_push_target(pawns, empty, :black)
     Utils.shift(single_push, :south) &&& empty &&& @rank5
   end
+
+  # Get the mask pawn attacks
+  @doc "Generate all squares that are attacked by pawns of a color at East direction"
+  @spec east_attacks(non_neg_integer(), :black | :white) :: non_neg_integer()
+  def east_attacks(pawns, :white), do: Utils.shift(pawns, :north_east)
+  def east_attacks(pawns, :black), do: Utils.shift(pawns, :south_east)
+
+  @doc "Generate all squares that are attacked by pawns of a color at West direction"
+  @spec west_attacks(non_neg_integer(), :black | :white) :: non_neg_integer()
+  def west_attacks(pawns, :white), do: Utils.shift(pawns, :north_west)
+  def west_attacks(pawns, :black), do: Utils.shift(pawns, :south_west)
+
+  # a bit-wise boolean instruction to combine those disjoint sets
+  @spec pawn_any_attacks(non_neg_integer(), :black | :white) :: non_neg_integer()
+  def pawn_any_attacks(pawns, color), do: bor( east_attacks(pawns, color), west_attacks(pawns, color) )
+
+  @spec pawn_dbl_attacks(non_neg_integer(), :black | :white) :: non_neg_integer()
+  def pawn_dbl_attacks(pawns, color), do: band( east_attacks(pawns, color), west_attacks(pawns, color) )
+
+  @spec pawn_single_attacks(non_neg_integer(), :black | :white) :: non_neg_integer()
+  def pawn_single_attacks(pawns, color), do: bxor( east_attacks(pawns, color), west_attacks(pawns, color) )
 end
